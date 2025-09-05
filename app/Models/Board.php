aca7e0466dd65a837a48cac09f2043eb8a7b4812
skyopcall <skyopcall@gmail.com>
2025-09-06 01:48:05 +0900

    created_at 만 등록 되도록 추가

--- app/Models/Board.php
+++ app/Models/Board.php
@@ -4,15 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Board extends Model
 {
-    use HasFactory;
-    use SoftDeletes;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = ['title', 'content', 'create_user'];
 
-     // updated_at 자동 관리 비활성화
-    public $timestamps = false;
+    public $timestamps = true; // 기본값 true
+    const UPDATED_AT = null;   // updated_at 사용 안함
 
 }
--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,14 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
 {
-    use HasFactory;
-    use SoftDeletes;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = ['board_id', 'content', 'author'];
 
-     // updated_at 자동 관리 비활성화
-    public $timestamps = false;
+    public $timestamps = true; // 기본값 true
+    const UPDATED_AT = null;   // updated_at 사용 안함
 }
